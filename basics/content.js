4f9545d493e8af8aafa642a1df09d8b292491994
Ofir Shapira <ofirs2003@gmail.com>
2020-10-26 14:56:37 +0200

    fix dict keys

--- basics/content.js
+++ basics/content.js
@@ -2,8 +2,29 @@
 console.log("hey everybody!");
 
 var dict = {
-    Gmail:{new : [], old:[]},
-    Maps:{new : [], old:[]},
-    Calendar:{new : [], old:[]},
-    Meet:{new : [], old:[]}
+    "Gmail":{"curr" : ["https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_server_1x.png","https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico",],
+     "old":["https://www.freepnglogos.com/uploads/logo-gmail-png/logo-gmail-png-gmail-icon-download-png-and-vector-1.png"],},
+    "Maps":{"curr" : ["https://www.gstatic.com/companion/icon_assets/maps_v2_2x.png",],
+     "old":[]},
+    "Calendar":{"curr" : ["https://www.gstatic.com/companion/icon_assets/fgdY29RxT2yHBfEODHlF70ZI3ytOaPoX/calendar_v3_2x.png"], 
+    "old":[]},
+    "Meet":{"curr": [], "old":[]}
+}
+
+
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+    var cond = false;
+    for(key in dict)
+    {
+
+        if(images[i].src in dict[key][curr])
+        {
+            images[i].src = dict[key][old][0];
+            cond = true;
+        }
+        if(cond)
+            break;
+
+    }
 }
