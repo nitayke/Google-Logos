5492f93570377f6fbc10b4e52cc9e72032b1194a
Ofir Shapira <ofirs2003@gmail.com>
2020-10-26 19:09:31 +0200

    added more pictures link

--- basics/content.js
+++ basics/content.js
@@ -14,12 +14,14 @@ var dict = {
      "old":["https://www.freepnglogos.com/uploads/logo-gmail-png/logo-gmail-png-gmail-icon-download-png-and-vector-1.png"]},
     
 	"Maps":{"curr" : ["https://www.gstatic.com/companion/icon_assets/maps_v2_2x.png",],
-     "old":[]},
+     "old":["https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Google_Maps_icon.svg/1200px-Google_Maps_icon.svg.png"]},
     
 	"Calendar":{"curr" : ["url(\"https://www.gstatic.com/companion/icon_assets/fgdY29RxT2yHBfEODHlF70ZI3ytOaPoX/calendar_v3_2x.png\")"], 
     "old":["url(\"https://ssl.gstatic.com/calendar/images/dynamiclogo/2x/cal_31_v2.png\")"]},
     
-    "Meet":{"curr": [], "old":[]},
+    "Meet":{"curr": ["https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_1x_icon_124_40_2373e79660dabbf194273d27aa7ee1f5.png",""], "old":["https://cdn.freelogovectors.net/wp-content/uploads/2020/07/google-meet-logo.png"]},
+    "Drive" : {"curr" : ["https://ssl.gstatic.com/images/branding/product/2x/hh_drive_96dp.png","https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_48dp.png"],"old" : ["https://www.pepperi.com/wp-content/uploads/2016/10/google-drive.png",]}
+
 
     
     
@@ -40,6 +42,7 @@ for (var i = 0; i < images.length; i++) {
             images[i].src = dict[key]["old"][0];
             cond = true;
         }
+        
         for (curr_url in dict[key]["curr"])
 		{
 			if (images[i].srcset.includes(curr_url))
