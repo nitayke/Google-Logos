7fab6007a84b2ec72c03855f89816a5e9508aa89
Nitay Kessner <nitayke@gmail.com>
2020-10-26 22:45:02 +0200

    Arranging the data

--- basics/content.js
+++ basics/content.js
@@ -7,25 +7,40 @@ function f()
 {
     var date = new Date();
     var dict = {
-    "Gmail":{"curr" : ["https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_server_1x.png",
-        "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico",
-        "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png",
-        "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_2x_rtl_r2.png",
-        "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_rtl_r2.png",],
-     "old":["https://www.freepnglogos.com/uploads/logo-gmail-png/logo-gmail-png-gmail-icon-download-png-and-vector-1.png"]},
+    "Gmail":{
+        "curr" : [
+            "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_server_1x.png",
+            "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico",
+            "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png",
+            "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_2x_rtl_r2.png",
+            "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_rtl_r2.png",],
+        "old":[
+            "https://www.freepnglogos.com/uploads/logo-gmail-png/logo-gmail-png-gmail-icon-download-png-and-vector-1.png"]},
     
-	"Maps":{"curr" : ["https://www.gstatic.com/companion/icon_assets/maps_v2_2x.png",],
-     "old":["https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Google_Maps_icon.svg/1200px-Google_Maps_icon.svg.png"]},
+	"Maps":{
+        "curr" : [
+            "https://www.gstatic.com/companion/icon_assets/maps_v2_2x.png",],
+        "old":[
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Google_Maps_icon.svg/1200px-Google_Maps_icon.svg.png"]},
     
-	"Calendar":{"curr" : ["https://www.gstatic.com/companion/icon_assets/fgdY29RxT2yHBfEODHlF70ZI3ytOaPoX/calendar_v3_2x.png"], 
-    "old":["https://ssl.gstatic.com/calendar/images/dynamiclogo/2x/cal_" + date.getDate() + "_v2.png"]},
+	"Calendar":{
+        "curr" : [
+            "https://www.gstatic.com/companion/icon_assets/fgdY29RxT2yHBfEODHlF70ZI3ytOaPoX/calendar_v3_2x.png"], 
+        "old":[
+            "https://ssl.gstatic.com/calendar/images/dynamiclogo/2x/cal_" + date.getDate() + "_v2.png"]},
 
-    "Meet":{"curr": ["https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_1x_icon_124_40_2373e79660dabbf194273d27aa7ee1f5.png"],
-    "old":["https://cdn.freelogovectors.net/wp-content/uploads/2020/07/google-meet-logo.png"]},
+    "Meet":{
+        "curr": [
+            "https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_1x_icon_124_40_2373e79660dabbf194273d27aa7ee1f5.png"],
+        "old":[
+            "https://cdn.freelogovectors.net/wp-content/uploads/2020/07/google-meet-logo.png"]},
     
-    "Drive" : {"curr" : ["https://ssl.gstatic.com/images/branding/product/2x/hh_drive_96dp.png",
-        "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_48dp.png"],
-    "old" : ["https://www.pepperi.com/wp-content/uploads/2016/10/google-drive.png",]}
+    "Drive" : {
+        "curr" : [
+            "https://ssl.gstatic.com/images/branding/product/2x/hh_drive_96dp.png",
+            "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_48dp.png"],
+        "old" : [
+            "https://www.pepperi.com/wp-content/uploads/2016/10/google-drive.png",]}
 
     }
     var images = document.getElementsByTagName('img');
